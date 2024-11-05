49cbf2722b1a7dbad2f9965504f537b130f395d4
Silas G. Ribeiro <guimaraes.silas2013@gmail.com>
2024-11-05 15:53:51 -0300

    product item component using gluestack

--- app/index.tsx
+++ app/index.tsx
@@ -1,7 +1,10 @@
-import { FlatList } from "react-native";
 import products from "../mocks/products.json";
 import { Product } from "../types/TProduct";
 import ProductListItem from "../components/productListItem";
+import { Box } from "@/components/ui/box";
+import { Fab, FabIcon } from "@/components/ui/fab";
+import { FlatList } from "react-native";
+import { Ionicons } from "@expo/vector-icons";
 
 const Home = () => {
   const renderItem = ({ item }: { item: Product }) => (
@@ -11,13 +14,27 @@ const Home = () => {
   const keyExtractor = (item: Product) => String(item.id);
 
   return (
-    <FlatList
-      contentContainerStyle={{ alignItems: "center", backgroundColor: "white" }}
-      numColumns={2}
-      data={products.products}
-      keyExtractor={keyExtractor}
-      renderItem={renderItem}
-    />
+    <Box>
+      <FlatList
+        numColumns={2}
+        contentContainerClassName="gap-2"
+        columnWrapperClassName="gap-2"
+        data={products.products}
+        keyExtractor={keyExtractor}
+        renderItem={renderItem}
+      />
+      <Fab
+        size="md"
+        placement="bottom right"
+        isHovered={false}
+        isDisabled={false}
+        isPressed={false}
+      >
+        <FabIcon
+          as={() => <Ionicons name="add" size={24} color="white" />}
+        ></FabIcon>
+      </Fab>
+    </Box>
   );
 };
 
--- components/productListItem/index.tsx
+++ components/productListItem/index.tsx
@@ -1,5 +1,6 @@
 import { Product } from "../../types/TProduct";
 import { Card } from "../ui/card";
+import { Divider } from "../ui/divider";
 import { Heading } from "../ui/heading";
 import { Image } from "../ui/image";
 import { Text } from "../ui/text";
@@ -7,14 +8,17 @@ import { VStack } from "../ui/vstack";
 
 const ProductListItem = ({ product }: { product: Product }) => {
   return (
-    <Card variant="outline" className="p-5 rounded-lg max-w-[170px] m-3">
+    <Card variant="outline" className="rounded-lg max-w-[180px] m-3">
       <Image
         source={{
           uri: product.thumbnail,
         }}
-        className="mb-6 h-[167px] w-full rounded-md"
+        className="mb-6 h-[170px] w-full rounded-md"
         alt="product related image"
       />
+
+      <Divider />
+
       <Heading size="md" className="mb-4">
         {product.title}
       </Heading>
@@ -22,7 +26,7 @@ const ProductListItem = ({ product }: { product: Product }) => {
         <Text size="sm">{product.description}</Text>
       </VStack>
       <Heading size="md" className="mb-4">
-        {product.price}
+        R$ {product.price}
       </Heading>
     </Card>
   );
