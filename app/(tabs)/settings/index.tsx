225e1c6c58a5292b9040c03779d0f5ac8a255f8a
Silas G. Ribeiro <guimaraes.silas2013@gmail.com>
2024-11-08 14:20:23 -0300

    add border-radius on settings screen

--- app/(tabs)/settings/index.tsx
+++ app/(tabs)/settings/index.tsx
@@ -12,33 +12,35 @@ import { VStack } from "@/components/ui/vstack";
 const Settings = () => {
   return (
     <Box className="flex-1">
-      <Box className="h-[200px] bg-blue-500" />
+      <Box className="h-[240px] bg-blue-500" />
 
-      <VStack className="items-center -mt-20 ">
-        <Avatar size="2xl">
-          <AvatarFallbackText>Jane Doe</AvatarFallbackText>
-          <AvatarImage
-            source={{
-              uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
-            }}
-          />
-        </Avatar>
+      <VStack className="rounded-t-xl bg-white -mt-20">
+        <VStack className="items-center -mt-20">
+          <Avatar size="2xl">
+            <AvatarFallbackText>Jane Doe</AvatarFallbackText>
+            <AvatarImage
+              source={{
+                uri: "https://images.unsplash.com/photo-1494790108377-be9c29b29330?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=687&q=80",
+              }}
+            />
+          </Avatar>
+        </VStack>
+        <VStack className="items-center">
+          <Text className="text-lg font-bold">João da Silva</Text>
+          <Text className="text-textLight500">joaodasilva@gmail.com</Text>
+        </VStack>
+        <VStack className="m-4">
+          <OptionItem icon="person" label="Meus dados" />
+          <OptionItem icon="notifications" label="Notificações" />
+          <OptionItem icon="document-text" label="Termos de uso" />
+        </VStack>
 
-        <Text className="text-lg font-bold">João da Silva</Text>
-        <Text className="text-textLight500">joaodasilva@gmail.com</Text>
+        <Box className="px-5">
+          <Button className="bg-red-500 rounded-lg">
+            <Text className="text-white font-bold">Sair da conta</Text>
+          </Button>
+        </Box>
       </VStack>
-
-      <VStack className="m-4 ">
-        <OptionItem icon="person" label="Meus dados" />
-        <OptionItem icon="notifications" label="Notificações" />
-        <OptionItem icon="document-text" label="Termos de uso" />
-      </VStack>
-
-      <Box className="px-5">
-        <Button className="bg-red-500 rounded-lg">
-          <Text className="text-white font-bold">Sair da conta</Text>
-        </Button>
-      </Box>
     </Box>
   );
 };
