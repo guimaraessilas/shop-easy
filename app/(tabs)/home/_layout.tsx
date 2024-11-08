de57b6328d537e03819984e2f095cd44d43c7896
Silas G. Ribeiro <guimaraes.silas2013@gmail.com>
2024-11-07 21:02:05 -0300

    setup top tab navigation

--- app/(tabs)/home/_layout.tsx
+++ app/(tabs)/home/_layout.tsx
@@ -1,11 +1,45 @@
-import { Stack } from "expo-router";
+import { SafeAreaView, useWindowDimensions } from "react-native";
+import { TabView, SceneMap, TabBar } from "react-native-tab-view";
+import MaleProducts from "./male";
+import FemaleProducts from "./female";
+import { useState } from "react";
+
+const renderScene = SceneMap({
+  male: MaleProducts,
+  female: FemaleProducts,
+});
+
+const routes = [
+  { key: "male", title: "Produtos Masculinos" },
+  { key: "female", title: "Produtos Femininos" },
+];
+
+const INITIAL_ROUTE_INDEX = 0;
 
 export default function HomeLayout() {
+  const layout = useWindowDimensions();
+  const [index, setIndex] = useState(INITIAL_ROUTE_INDEX);
+
   return (
-    <Stack
-      screenOptions={{
-        headerShown: false,
-      }}
-    />
+    <SafeAreaView style={{ flex: 1 }}>
+      <TabView
+        navigationState={{ index, routes }}
+        renderScene={renderScene}
+        onIndexChange={setIndex}
+        initialLayout={{ width: layout.width }}
+        renderTabBar={(props) => (
+          <TabBar
+            {...props}
+            style={{ backgroundColor: "#fff" }}
+            indicatorStyle={{ backgroundColor: "#2567E8" }}
+            labelStyle={{
+              color: "#000",
+            }}
+            activeColor="#000"
+            inactiveColor="#000"
+          />
+        )}
+      />
+    </SafeAreaView>
   );
 }
--- app/_layout.tsx
+++ app/_layout.tsx
@@ -1,6 +1,5 @@
 import "@/global.css";
 import { GluestackUIProvider } from "@/components/ui/gluestack-ui-provider";
-
 import { Stack } from "expo-router";
 import { QueryClientProvider } from "@tanstack/react-query";
 import queryClient from "@/utils/queryClient";
@@ -11,8 +10,7 @@ const RootLayout = () => {
       <GluestackUIProvider>
         <Stack
           screenOptions={{
-            headerBackTitleVisible: false,
-            headerTitle: "",
+            headerShown: false,
           }}
         />
       </GluestackUIProvider>
